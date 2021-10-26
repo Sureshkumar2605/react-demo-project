9fbf45f5ba989266e5bb41003fbc3464a8e76d15
unknown <sureshkumard26@gmail.com>
2021-10-26 15:39:27 +0530

    My react app first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import GetPost from './GetPost';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <GetPost/>
+    </div>
+  );
+}
+
+export default App;
